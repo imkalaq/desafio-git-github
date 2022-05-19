b6ce1452884ec7ed289031c12f18a903de280ee9
Kalaq <ramonlima64@gmail.com>
2022-05-19 13:24:54 -0300

    Novos exercícios práticos

---
+++ Desafio DIO/Desenvolvimento Web/index2.php
@@ -0,0 +1,34 @@
+<html>
+
+	<head>
+		<title>Meu Primeiro Site em PHP!</title>
+		
+		<script scr="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+		
+		<style type="text/css">
+			.linha {
+				font-weight: bold;
+				color: red;
+				padding-left: 10px;
+				line-height: 50px;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<?php
+			for ( $i = 0 ; $i < 10 ; $i++ ) {
+				print( "<span class=\"linha\">Linha número " . $i . "</span><br />");
+			}
+		?>
+	</body>
+	
+	<script type="text/javascript">
+		$(document).ready(function()	{
+			alert ("Woohoo");
+		});
+	</script>
+	
+</html>
+	
+	
